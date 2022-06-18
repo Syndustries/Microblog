2a63912b2bed7aba92d3c40c0632f59b59c0d077
Zenoprojekt <107429180+Zenoprojekt@users.noreply.github.com>
2022-06-17 23:08:22 -0400

    Chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,18 @@
+from flask import render_template
 from app import app
+
 @app.route("/")
 @app.route("/index")
 def index ( ) :
-        return "Goodbye, World!"
+        user = {'username': 'Storm'}
+        posts = [
+        {
+                'author' : {'username': 'Susan'},
+                'body' : 'The Avengers movie was so cool!'
+        },
+        {
+                'author': {'username': 'Storm'},
+                'body' : 'Something or other as a test placeholder'
+        }
+        ]
+        return render_template('index.html', user=user, posts=posts)
